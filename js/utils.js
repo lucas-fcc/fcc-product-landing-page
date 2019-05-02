99e7bc6021cbf6d71f35a89e5e2f2016dd2991ae
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2019-05-01 21:40:59 -0300

    scrollspy and smooth scroll

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 
+  $('#nav-bar a').on('click', smoothScroll);
+  $('#read-more').on('click', smoothScroll);
+
+  function smoothScroll(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800);
+    }
+  }
+
 });
